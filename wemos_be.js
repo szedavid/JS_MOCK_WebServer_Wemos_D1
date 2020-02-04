77bd60bc018ceadef9d089e9f40790ffe29befa0
Szegedi Dávid <szegedi.david@innobyte.hu>
2020-02-04 16:11:31 +0100

    Servo support + some path changes

--- wemos_be.js
+++ wemos_be.js
@@ -8,6 +8,7 @@ const app = express();
 let port = 3000;
 
 let ledState = false;
+let servoAngle = 0;
 
 app.use(cors());
 
@@ -15,7 +16,7 @@ app.get('/restText', (req, res) => {
     res.send(Math.random(1000).toFixed(2).toString());
 });
 
-app.get('/rest', (req, res) => {
+app.get('/monitor', (req, res) => {
     res.send(JSON.stringify(getData()));
 });
 
@@ -31,7 +32,6 @@ app.get('/rest-array', (req, res) => {
 function getData(){
     let now = new Date();
     let myObj = {
-        ledState: ledState,
         // time: now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds(),
         time: (now.getHours()*3600 + now.getMinutes()*60 + now.getSeconds())*1000 + now.getMilliseconds(),
         value: Math.random(1000)
@@ -40,17 +40,36 @@ function getData(){
 }
 
 // LED
-app.get('/on', (req, res) => {
-    ledState = true;
-    res.send(JSON.stringify({ledState: ledState}));
+// app.get('/on', (req, res) => {
+//     ledState = true;
+//     res.send(JSON.stringify({ledState: ledState}));
+// });
+
+// app.get('/off', (req, res) => {
+//     ledState = false;
+//     res.send(JSON.stringify({ledState: ledState}));
+// });
+
+// todo delete above
+app.post('/led/:ledState', (req, res) => {
+    ledState = Boolean(JSON.parse(req.params.ledState));
+    res.send(JSON.stringify({ledState: ledState, servoAngle: servoAngle}));
 });
 
-app.get('/off', (req, res) => {
-    ledState = false;
-    res.send(JSON.stringify({ledState: ledState}));
+// SERVO
+app.get('/controller', function(req, res) {
+    res.send(JSON.stringify({ledState: ledState, servoAngle: servoAngle}));
+});
+
+app.post('/servo/:angle', function(req, res) {
+    servoAngle = req.params.angle;
+    res.send(JSON.stringify({ledState: ledState, servoAngle: servoAngle}));
 });
 
 
+
+
+// START
 app.listen(port, () => {
     console.log('Server listening on port: ' + port);
 });
